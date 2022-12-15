b3aecc8949b71b536e6129f416cdf08593dc13bc
ingagrigoryan <114287294+ingagrigoryan@users.noreply.github.com>
2022-12-15 21:17:46 +0400

    Update vector_to_matrix.cpp

--- Inga/vector_to_matrix.cpp
+++ Inga/vector_to_matrix.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
 #include <math.h>
-#include "transpos.h"
+#include "vector_to_matrix.h"
 
 using namespace std;
 
-transpos::transpos(int x){
-  x = N;
+Vec::Vec(int X){
+  x = X;
 }
+
+void Vec::setArr(){
+  for(int i = 0; i < x; i++){
+    arr[i] = i + 3;
+  }
+  for(int i = 0; i < x; i++){
+    cout << arr[i] << "  ";
+  }
+  cout << endl;
+}
+
+void Vec::getA(){
+  k = sqrt(x);
+  for(int i = 0; i < k; i++){
+    for(int j = 0; j < k; j++){
+      A[i][j] = arr[j + i * k];
+    }
+  }
+  
+  for(int i = 0; i < k; i++){
+    for(int j = 0; j < k; j++){
+      cout << A[i][j] << " ";
+    }
+  }
+  cout << endl;
+}
+
+void Vec::getB(){
+  k = sqrt(x);
+  for(int i = 0; i < k; i++){
+    for(int j = 0; j < k; j++){
+      B[i][j] = A[i][j];
+    }
+  }
+  
+  for(int i = 0; i < k; i++){
+    for(int j = 0; j < k; j++){
+      cout << B[i][j] << " ";
+    }
+  }
+  cout << endl;
+}
+
+
